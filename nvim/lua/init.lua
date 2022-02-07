5e0911bd65b2e0b0e6f2b64e3ccaeb9ef9e986b6
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-02-07 23:09:31 +0100

    Setup neovim init.lua

---
+++ nvim/lua/init.lua
@@ -0,0 +1,1 @@
+require('disable_builtin')
--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -2,3 +2,6 @@ ln -vsf ~/dotfiles/.zshrc ~/.zshrc
 
 ln -vsf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf 
 ln -vsf ~/dotfiles/kitty/themes/nord.conf ~/.config/kitty/nord.conf 
+
+mkdir -p ~/.config/nvim
+ln -vsf ~/dotfiles/nvim/**/* ~/.config/nvim
