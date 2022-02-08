5f594608550f3530ac37cf723d8d4f911f38a08e
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-02-08 10:37:42 +0100

    Install nvim-tree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,3 +12,11 @@ use { -- TODO: Add description
 	'williamboman/nvim-lsp-installer',
 	config = [[ require('plugins/nvim-lsp-installer')]]
 }
+
+use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+}
