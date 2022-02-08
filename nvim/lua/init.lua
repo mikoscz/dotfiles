602caab732fd86cd791baedde710af2eb0b1af87
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-02-08 10:37:55 +0100

    Add nvim indent settings

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -8,5 +8,18 @@ vim.wo.number = true
 --Enable mouse mode
 vim.o.mouse = 'a'
 
+-- indent
 --Use system clippboard
 vim.o.clipboard = "unnamedplus"
+
+-- TODO: Add description
+vim.b.tabstop = 2
+
+-- TODO: Add description
+vim.b.softtabstop = 2
+
+-- TODO: Add description
+vim.b.shiftwidth = 2
+
+-- TODO: Add description
+vim.b.expandtab = true
